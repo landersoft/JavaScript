c7b0d6d8e6437ca139ab414f5b825147ccb4921d
landersoft <landersoft@gmail.com>
2021-09-12 19:34:07 -0300

    exportar desde otro archivo.

---
+++ Fundamentos/app.js
@@ -0,0 +1,8 @@
+
+// se importa o y se asigna a una constante. 
+const vehi = require('./vehiculos')
+
+// se imprime en consola. 
+vehi.forEach(item =>{
+    console.log(item)
+})
---
+++ Fundamentos/vehiculos.js
@@ -0,0 +1,6 @@
+
+// se define una lista y se explorta
+const vehiculos = ['auto','camion','tractor','motocicleta','auto'];
+
+//exportar la lista vehiculos
+module.exports = vehiculos;
